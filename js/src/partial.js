7d2f54c4bb9954f2cf9d44623e946de688976953
dylan-7 <wuxiewang@gmail.com>
2016-11-02 12:29:21 +0800

    Site updated: 2016-11-02 12:29:20

--- js/src/partial.js
+++ js/src/partial.js
@@ -3,6 +3,8 @@
 */
 
 $(function() {
+  "use strict";
+
   // setTimeout(function() {
   //   if($("#emotify-widget-frame")) {
   //     $("#emotify-container").addClass('emotify-mask');
@@ -14,4 +16,16 @@ $(function() {
   if($("[title='知乎']")) {
     $("[title='知乎']").find('i').addClass('iconfont icon-zhihu');
   }
+
+  try{
+    if(window.console && window.console.log) {
+      console.log(`%c` + `亲，要不要到火星做做客？`, `font-size:1.8em;
+                padding-left:2em;
+                padding-top:2em;
+                padding-bottom:2em;
+                line-height:4em;
+                background:url("http://7xwkxy.com1.z0.glb.clouddn.com/ufo-3.png") no-repeat left center;
+                background-size:10%;`);
+    }
+  } catch(e) {}
 })
