6895fc83e5031a67fa22ce39941fd2a37eda52d6
dylan-7 <wuxiewang@gmail.com>
2016-11-03 14:22:34 +0800

    Site updated: 2016-11-03 14:22:31

--- js/src/partial.js
+++ js/src/partial.js
@@ -19,15 +19,15 @@ $(function() {
 
   try{
     if(window.console && window.console.log) {
-      console.log(`%c` + `亲，要不要到火星做做客？`, `font-size:1.4em;
-                font-family: "microsoft yahei";
-                padding-left:2.2em;
-                padding-top:2em;
-                padding-bottom:2em;
-                line-height:4em;
-                background:url("http://7xwkxy.com1.z0.glb.clouddn.com/ufo-3.png") no-repeat left center;
-                background-size:12%;`);
-      console.log(`%c` + `http://www.dylan100.com`,`color:gray;`);
+      console.log('%c' + '亲，要不要到火星做做客？', 'font-size:1.4em;\
+                padding-left:2.2em;\
+                padding-top:2em;\
+                padding-bottom:2em;\
+                line-height:4em;\
+                background:url("http://7xwkxy.com1.z0.glb.clouddn.com/ufo-3.png")\
+                        no-repeat left center;\
+                background-size:12%;');
+      console.log("%c" + "http://www.dylan100.com","color:gray;");
     }
   } catch(e) {}
 })
